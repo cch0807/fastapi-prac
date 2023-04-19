4eedd64bb985b1dc321896bcadd0302b9a615702
choich0807 <ch.choi@aizen.co>
2023-04-19 11:51:32 +0900

    feat: question curd init

--- backend/migrations/versions/4addcf192f49_.py
+++ backend/migrations/versions/4addcf192f49_.py
@@ -22,7 +22,7 @@ def upgrade() -> None:
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('subject', sa.String(), nullable=False),
     sa.Column('content', sa.Text(), nullable=False),
-    sa.Column('create_dat', sa.DateTime(), nullable=False),
+    sa.Column('create_date', sa.DateTime(), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('answer',
