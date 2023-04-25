f4e86076ae56458d5c84163b3234754020ed88f8
choich0807 <ch.choi@aizen.co>
2023-04-25 10:25:51 +0900

    feat: user signup front & backend

--- backend/domain/user/user_router.py
+++ backend/domain/user/user_router.py
@@ -1,18 +1,29 @@
+from datetime import timedelta, datetime
+
 from fastapi import APIRouter, HTTPException
 from fastapi import Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 from sqlalchemy.orm import Session
 from starlette import status
 
 from backend.database import get_db
 from backend.domain.user import user_crud, user_schema
+from backend.domain.user.user_crud import pwd_context
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+
 
 router = APIRouter(
     prefix="/api/user",
 )
 
+
 @router.post("/create", status_code=status.HTTP_204_NO_CONTENT)
 def user_create(_user_create: user_schema.UserCreate, db: Session = Depends(get_db)):
     user = user_crud.get_existing_user(db, user_create=_user_create)
     if user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="이미 존재하는 사용자입니다.")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="이미 존재하는 사용자입니다."
+        )
     user_crud.create_user(db=db, user_create=_user_create)
