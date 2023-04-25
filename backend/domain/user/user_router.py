347f3717ae85ffac058343025a27cee33659a219
choich0807 <ch.choi@aizen.co>
2023-04-25 10:46:11 +0900

    feat: user login & logout

--- backend/domain/user/user_router.py
+++ backend/domain/user/user_router.py
@@ -12,6 +12,8 @@ from backend.domain.user import user_crud, user_schema
 from backend.domain.user.user_crud import pwd_context
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+SECRET_KEY = "4c45af753b09a80b77c50b74c67e092aadf911de9523719dcd887e193655c985"
+ALGORITHM = "HS256"
 
 
 router = APIRouter(
@@ -27,3 +29,30 @@ def user_create(_user_create: user_schema.UserCreate, db: Session = Depends(get_
             status_code=status.HTTP_409_CONFLICT, detail="이미 존재하는 사용자입니다."
         )
     user_crud.create_user(db=db, user_create=_user_create)
+
+
+@router.post("/login", response_model=user_schema.Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    # check user and password
+    user = user_crud.get_user(db, form_data.username)
+    if not user or not pwd_context.verify(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    # make access token
+    data = {
+        "sub": user.username,
+        "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+    }
+    access_token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "username": user.username,
+    }
