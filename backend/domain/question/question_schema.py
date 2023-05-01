369d9ec37f76d6c1d752ea80fb299ac143b87f34
choich0807 <ch.choi@aizen.co>
2023-05-01 23:45:58 +0900

    feat: save answer

--- backend/domain/question/question_schema.py
+++ backend/domain/question/question_schema.py
@@ -3,6 +3,8 @@ import datetime
 from pydantic import BaseModel, validator
 
 from backend.domain.answer.answer_schema import Answer
+from backend.domain.user.user_schema import User
+
 
 class Question(BaseModel):
     id: int
@@ -10,20 +12,23 @@ class Question(BaseModel):
     content: str
     create_date: datetime.datetime
     answers: list[Answer] = []
+    user: User | None
 
     class Config:
         orm_mode = True
+
 
 class QuestionCreate(BaseModel):
     subject: str
     content: str
 
-    @validator('subject', 'content')
+    @validator("subject", "content")
     def not_empty(cls, v):
         if not v or not v.strip():
-            raise ValueError('빈 값은 허용되지 않습니다.')
+            raise ValueError("빈 값은 허용되지 않습니다.")
         return v
-    
+
+
 class QuestionList(BaseModel):
     total: int = 0
     question_list: list[Question] = []
--- backend/domain/user/user_schema.py
+++ backend/domain/user/user_schema.py
@@ -21,6 +21,15 @@ class UserCreate(BaseModel):
         return v
 
 
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
 class Token(BaseModel):
     access_token: str
     token_type: str
