a7ab735dfff90a83cb8096a4d30bc8117c504b6d
choich0807 <ch.choi@aizen.co>
2023-05-02 16:29:03 +0900

    feat: save & show answer

--- backend/domain/question/question_crud.py
+++ backend/domain/question/question_crud.py
@@ -1,23 +1,29 @@
 from datetime import datetime
 
 from backend.domain.question.question_schema import QuestionCreate
-from backend.models import Question
+from backend.models import Question, User
 from sqlalchemy.orm import Session
 
+
 def get_question_list(db: Session, skip: int = 0, limit: int = 10):
-    _question_list = db.query(Question)\
-        .order_by(Question.create_date.desc())
-    
+    _question_list = db.query(Question).order_by(Question.create_date.desc())
+
     total = _question_list.count()
     _question_list = _question_list.offset(skip).limit(limit).all()
-    return total, _question_list # (전체 건수, 페이징 적용된 질문 목록)
+    return total, _question_list  # (전체 건수, 페이징 적용된 질문 목록)
+
 
 def get_question(db: Session, question_id: int):
     question = db.query(Question).get(question_id)
     return question
 
-def create_question(db: Session, question_create: QuestionCreate):
-    db_question = Question(subject=question_create.subject,content=question_create.content ,create_date=datetime.now())
+
+def create_question(db: Session, question_create: QuestionCreate, user: User):
+    db_question = Question(
+        subject=question_create.subject,
+        content=question_create.content,
+        create_date=datetime.now(),
+        user=user,
+    )
     db.add(db_question)
     db.commit()
-
